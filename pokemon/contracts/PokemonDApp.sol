cf660350b05f063b575e8537b30dfb337c760bba
aniketshrimal <aniketshrimal98@gmail.com>
2018-11-14 23:42:09 +0530

    final app

--- pokemon/contracts/PokemonDApp.sol
+++ pokemon/contracts/PokemonDApp.sol
@@ -1,64 +1,147 @@
 pragma solidity ^0.4.17;
-pragma experimental ABIEncoderV2;
+// pragma experimental ABIEncoderV2;
 
 contract PokemonDApp {
     struct pokemon {
         uint id;
-        uint value;
+        uint level;
         string name;
     }
-    mapping (uint => pokemon) pokemons;
-    uint pokemonCount = 0;
-    event newPokemon(uint id);
+
+    struct tradepok{
+        uint id;
+        address account;
+        uint cost;
+        // uint cost;        
+    }
+    mapping (uint => tradepok) public tradepoks;
+    uint public tradepokcount;
+    mapping (uint => pokemon) public pokemons;
+    uint public pokemonCount ;
     // pokemons[0] = pokemon(0,1,"Squirtle"); 
     // pokemons[1] = pokemon(1,1,"Charmander");
+    
+    uint playerCount ;
     function addPokemon(string _name) private {
         pokemonCount ++;
         pokemons[pokemonCount] = pokemon(pokemonCount, 1, _name);
         
     }
+    mapping(address => mapping (uint => uint)) ownpokemon;
+    mapping(address => uint) pokecount;
     constructor() public {
-        addPokemon("Charmander");
+        tradepokcount = 0;
+        playerCount = 0;
+        pokemonCount =0;
+        addPokemon("Pikachu");
         addPokemon("Bulbasaur");
-        addPokemon("xyz");
+        addPokemon("Charmander");
+        addPokemon("Squirtle");
     
     }
-    int playerCount = 0;
+
     mapping (address  => pokemon[]) pokemonMap;
-    mapping (int => address) playermap;
+    mapping (uint => address) playermap;
     mapping (address => bool) registered;
-    function register() public returns(uint){
+    mapping(address => uint) pendingReturns;
+    function register() public payable {
         require(registered[msg.sender]!=true,"user already registered");
+        pokemon storage p = pokemons[playerCount%4+1];
         playerCount++;
+        pokecount[msg.sender]=0;
         playermap[playerCount] = msg.sender;
-        pokemonMap[msg.sender].push(pokemons[random(uint(3))]);
+        ownpokemon[msg.sender][pokecount[msg.sender]] = (p.id);
+        pokecount[msg.sender]++;
         registered[msg.sender] = true;
-        return 1;
+        pendingReturns[msg.sender]=msg.value;
     }
-    
-    function getPokemons() public view returns(uint[]) {
-        uint[] temp;
-        for(uint i=0;i<pokemonMap[msg.sender].length;i++) {
-            temp.push(pokemonMap[msg.sender][i].id);
+
+    function catching(uint i) public{
+        require(registered[msg.sender]==true,"user not registered");
+        pokemon storage p = pokemons[i];
+        ownpokemon[msg.sender][pokecount[msg.sender]] = (p.id);
+        pokecount[msg.sender]++;
+    }
+
+    function trade(uint id,uint cost) public{
+        require(registered[msg.sender]==true,"user not registered");
+        uint p  = pokecount[msg.sender];
+        uint i = 0;
+        for(i=0;i<p;i++)
+        {
+            if(ownpokemon[msg.sender][i]==id)
+            {
+                for(uint j=i;j<p-1;j++)
+                {
+                    ownpokemon[msg.sender][j]=ownpokemon[msg.sender][j+1];
+                }
+                break;
+            }
         }
-        //return (pokemonMap[addr].id,pokemonMap[addr].value,pokemonMap[addr].name);
-        return temp;
+        delete ownpokemon[msg.sender][p-1];
+        pokecount[msg.sender]--;
+        tradepoks[tradepokcount] = tradepok({id:id,account:msg.sender,cost:cost}); 
+        tradepokcount++;
     }
-    
-    function isRegistered() public view returns(bool) {
+
+    function buy(uint t) public payable{
+        require(registered[msg.sender]==true,"user not registered");
+        
+        uint id  = tradepoks[t].id;
+        
+        uint cost  = tradepoks[t].cost;
+        
+        require(pendingReturns[msg.sender]>=cost,"Insufficient balance");
+        address addr = tradepoks[t].account;
+        for(uint i=t;i<tradepokcount-1;i++)
+        {
+            tradepoks[i]=tradepoks[i+1];
+        }
+        delete tradepoks[tradepokcount-1];
+        tradepokcount--;
+        ownpokemon[msg.sender][pokecount[msg.sender]] = id;
+        pendingReturns[msg.sender]-=cost;
+        pendingReturns[addr]+=cost;
+        pokecount[msg.sender]++;
+    }
+    function getPokTradeCount() public view returns(uint)
+    {
+        return tradepokcount;
+    }
+    function getPokTrade(uint i) public view returns(uint, uint , string,uint)
+    {
+        return (pokemons[tradepoks[i].id].id,pokemons[tradepoks[i].id].level,pokemons[tradepoks[i].id].name,tradepoks[i].cost);
+    }
+
+    function getPok(uint i) public view returns(uint,uint,string){
+        return (pokemons[i].id,pokemons[i].level,pokemons[i].name);
+    }
+    function getOwnCount() public view returns(uint){
+        return pokecount[msg.sender];
+    }
+    function getPokemonCount() public view returns (uint ){
+        return pokemonCount;
+    }
+    function getOwnPok(uint i) public view returns(uint ,uint ,string ){
+        return (pokemons[ownpokemon[msg.sender][i]].id,pokemons[ownpokemon[msg.sender][i]].level,pokemons[ownpokemon[msg.sender][i]].name);
+    }  
+    function getregist() public view returns (bool){
         return registered[msg.sender];
     }
-
-    function randomDrop() public {
-        uint rand = random(uint(3));
-        pokemonMap[msg.sender].push(pokemons[rand]);
-        newPokemon(rand);
+    function random(uint num) returns(uint) {
+        return uint256(keccak256(block.timestamp, block.difficulty))%num;
     }
-
-    function random(uint num) public view returns(uint) {
-        return uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty)))%num;
+    function getmoney() public view returns (uint){
+        return pendingReturns[msg.sender];
     }
     
-    
-    
+    function withdraw() 
+    public
+    payable
+    {
+        require(pendingReturns[msg.sender]>0,"No pending returns");
+        uint Balance = pendingReturns[msg.sender];       
+        (msg.sender).transfer(Balance);
+        pendingReturns[msg.sender]=0;
+    }
 }
--- pokemon/src/js/app.js
+++ pokemon/src/js/app.js
@@ -4,137 +4,257 @@ App = {
   account : '0x0',
 
   init: function() {
-    return App.initWeb3();
+	return App.initWeb3();
   },
 
   initWeb3: async function() {
-    if(typeof web3 !== 'undefined') {
-      App.web3Provider = web3.currentProvider;
-      web3 = new Web3(web3.currentProvider);
-    }
-    else {
-      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
-      web3 = new Web3(web3.currentProvider);      
-    }
-    return App.initContract();
+	if(typeof web3 !== 'undefined') {
+	  App.web3Provider = web3.currentProvider;
+	  web3 = new Web3(web3.currentProvider);
+	}
+	else {
+	  App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
+	  web3 = new Web3(web3.currentProvider);      
+	}
+	return App.initContract();
   },
 
   initContract: function() {
-    $.getJSON("PokemonDApp.json", function(pokemon) {
-      App.contracts.PokemonDApp = TruffleContract(pokemon);
-      App.contracts.PokemonDApp.setProvider(App.web3Provider);
-      App.listenForEvents();  
-      return App.render();
+	$.getJSON("PokemonDApp.json", function(pokemon) {
+		  App.contracts.PokemonDApp = TruffleContract(pokemon);
+		  App.contracts.PokemonDApp.setProvider(App.web3Provider);
+		  return App.check();
 
-    });
+	});
   },
 
   render: async function() {
-    web3.eth.getCoinbase(function(err, account) {
-      if(err === null) {
-        App.account = account;
-        $("#accountAddress").html("Your Account: " + account);
-      }});
-    var inst = await App.contracts.PokemonDApp.deployed();
-    var isRegistered = await inst.isRegistered();
-    console.log(isRegistered);
-    if(isRegistered == true) {
-      $('#register').hide();
-      $('#loader').hide();
-      return App.display();
-    }
+	var loader = $("#loader");
+	var content = $("#content");
+	var show1 = $("#show1");
+	var show2 = $("#show2");
+	var show3 = $("#show3");
+	loader.show();
+	content.hide();
+	show1.hide();
+	show2.hide();
+	show3.hide();
+
+
+	try{
+		  var inst  = await  App.contracts.PokemonDApp.deployed();
+		
+			await $("#register").hide();
+			$("#idpok").empty();
+			$("#idpok1").empty();
+			$("#idpok2").empty();
+			var p = await inst.getOwnCount({from:App.account});
+			for(var i=0;i<p;i++){
+				var pokemon = await inst.getOwnPok(i,{from:App.account});
+				
+				var id = pokemon[0];
+				var name = pokemon[2];
+				var level = pokemon[1];
+				var temp = "<div class = 'card' ><img src= /images/"  + id + level + ".jpeg alt="+id+level+" style='width:40%'><h2>" + name + "</h2><p>Level:" + level + "</p><p><button onclick=App.trade("+id+")>Trade</button></p><p><input type='text' size='25' ' id ='cost"+id+"'></p></div>";
+				$("#idpok").append(temp);
+  				
+			}
+
+			var q = await inst.getPokemonCount();
+			for(i=1;i<=q;i++)
+			{
+				var val=1;
+				for(j = 0; j<p;j++)
+				{
+					var pokemon = await inst.getOwnPok(j,{from:App.account});
+					var id = pokemon[0];
+					if(id==i)
+					{
+						val=0;
+						break;
+					}
+				}
+				var s = Math.floor(Math.random() * 100);
+
+				if(val && s%2==1 )
+				{
+					var pokemon = await inst.getPok(i,{from:App.account});
+					var id = pokemon[0];
+					var name = pokemon[2];
+					var level = pokemon[1];
+					var temp = "<div class = 'card' ><img src= /images/"  + id + level + ".jpeg alt="+id+level+" style='width:40%'><h2>" + name + "</h2><p>Level:" + level + "</p><p><button onclick=App.catching("+id+")>Catch</button></p></div>";
+					$("#idpok1").append(temp);				
+				}
+			}
+
+			var r= await inst.getPokTradeCount();
+			for(var i=0;i<r;i++)
+			{
+				var pokemon = await inst.getPokTrade(i);
+				var id = pokemon[0];
+				var name = pokemon[2];
+				var level = pokemon[1];
+				var cost  = pokemon[3];
+				var temp = "<div class = 'card' ><img src= /images/"  + id + level + ".jpeg alt="+id+level+" style='width:40%'><h2>" + name + "</h2><p>Level:" + level + "</p><p><h2>price: "+cost+"</h2></p><p><button onclick=App.buy("+i+")>Buy</button></p></div>";
+				$("#idpok2").append(temp);					
+			}
+			
+		  
+		// }
+		loader.hide();
+		content.show();
+		show1.show();
+		show2.show();
+		show3.show();
+
+
+
+	}
+	catch(err){
+	  console.log(err);
+	}
   },
 
-  display: async function() {
-    console.log('clicked');
-    var loader = $("#loader");
-    var content = $("#content");
-    loader.show();
-    content.hide();
-    var inst = await App.contracts.PokemonDApp.deployed();
-    try{
-      var pokemons = await inst.getPokemons({from:App.account});
-      var all_pokemons = $("#all_pokemons");
-      all_pokemons.empty();
-      var name = ['Charmander','Squirtle','Bulbasaur'];
-      var level = [1,2,3];
-      for(var i=0;i<pokemons.length;i++) {
-        var id = pokemons[i];
-        console.log(id);
-          var temp = "<tr><td>" + name[id] + "</td><td>" + level[id] + "</td></tr>";
-          all_pokemons.append(temp);
-      }
-      loader.hide();
-      content.show();
-    }
-    catch(err) {
-      console.log(err);
-    }
-  },
+  catching: async function(i){
+  	console.log("clicked");
+	var inst = await App.contracts.PokemonDApp.deployed();
+	try{
+	  await inst.catching(i,{from:App.account});
+	  alert('Successfully catched');
+	  return App.render();
+	}
+	catch(err){
+		console.log(err);
+		alert('Not able to catch');
+		return App.render();
+		// return App.render();
+		} 
+	},
+  
 
+  trade: async function(i){
+  		console.log('clicked');
+  		var inst = await App.contracts.PokemonDApp.deployed();
+  		try{
+
+  			var cost = $("#cost"+i).val();
+  			console.log(cost);
+  			await inst.trade(i,cost,{from:App.account});
+	  		alert('Successfully traded');
+	  		return App.render();
+  		}		
+
+		catch(err){
+			console.log(err);
+			alert('Not able to trade');
+			return App.render();
+		// return App.render();
+		} 				
+  },
+  buy: async function(i){
+  		console.log('clicked');
+  		var inst = await App.contracts.PokemonDApp.deployed();
+  		try{
+  			await inst.buy(i,{from:App.account});
+	  		alert('Successfully bought');
+	  		return App.render();
+  		}		
+
+		catch(err){
+			console.log(err);
+			alert('Not able to buy');
+			return App.render();
+		// return App.render();
+		}  
+	},
   register: async function() {
-    console.log('clicked');
-    var loader = $("#loader");
-    var content = $("#content");
-    loader.show();
-    content.hide();
-    var inst = await App.contracts.PokemonDApp.deployed();
-    try{
-      await inst.register({from:App.account});
-      var pokemons = await inst.getPokemons({from:App.account});
-      var all_pokemons = $("#all_pokemons");
-      all_pokemons.empty();
-      var name = ['Charmander','Squirtle','Bulbasaur'];
-      var level = [1,2,3];
-      for(var i=0;i<pokemons.length;i++) {
-        var id = pokemons[i];
-        console.log(id);
-          var temp = "<tr><td>" + name[id] + "</td><td>" + level[id] + "</td></tr>";
-          all_pokemons.append(temp);
-      }
-      loader.hide();
-      content.show();
-      alert('Successfully Registered');
+	console.log('clicked');
+	var loader = $("#loader");
+	var content = $("#content");
+	loader.show();
+	content.hide();
+	var inst = await App.contracts.PokemonDApp.deployed();
+	try{
+	  await inst.register({from:App.account,value: web3.toWei(0.0000000000000001,'ether')});
+	  alert('Successfully registered');
+	  return App.render();
+	}
+	catch(err){
+		console.log(err);
+		alert('Insufficient funds');
+		return App.check();
+		// return App.render();
+		} 
+	},
+	
+	wallet: async function(){
+		console.log('clicked');
+		try{
 
-    }
-    catch(err) {
-      console.log(err);
-    }
+		var inst = await App.contracts.PokemonDApp.deployed();
+		console.log('clicked');
+		var p  =await inst.getmoney({from:App.account});
+	  	console.log(p);
+	  	alert("Balance:"+p);
 
-  },
-  dropPokemon: async function() {
-    
-    var inst = await App.contracts.PokemonDApp.deployed();
-    var isRegistered = await inst.isRegistered();
-    // console.log('Pokemon Drop'+isRegistered);
-    if(isRegistered == true) {
-        console.log('New Pokemon');
+	  	return App.render();
+		}
+		catch(err){
+			alert('No pending returns');
+			return App.render();
+		}
+	},
+  check: async function(){
+	var loader = $("#loader");
+	var content = $("#content");
+	var show1 = $("#show1");
+	var show2 = $("#show2");
+	var show3 = $("#show3");
+	loader.show();
+	content.hide();
+	show1.hide();
+	show2.hide();
+	show3.hide();
 
-        try {
-        var temp = await inst.randomDrop();
-      }
-      catch(err) {
-        console.log(err);
-      }  
-    }
-  },
-  listenForEvents: function() {
-  App.contracts.PokemonDApp.deployed().then(function(instance) {
-    instance.newPokemon({}, {
-      fromBlock: 0,
-      toBlock: 'latest'
-    }).watch(function(error, event) {
-      console.log("event triggered", event)
-      // Reload when a new vote is recorded
-      App.render();
-    });
-  });
+	content.hide();
+	await web3.eth.getCoinbase(function(err, account) {
+	  if(err === null) {
+		App.account = account;
+		acc = account;
+		$("#accountAddress").html("Your Account: " + account);
+	  }
+	});
+	try{
+		var inst = await App.contracts.PokemonDApp.deployed();
+		var p = 1;
+		console.log(p);
+		var p = await inst.getregist({from : App.account});
+		console.log(p);
+		if(p===true)
+			return App.render();
+		else
+		{
+		  content.show();
+		  loader.hide();
+		  show1.hide();
+			show2.hide();
+			show3.hide();
+
+
+		}
+	}
+	catch(err){
+		console.log(err);
+	}
   }
+
 };
+
+
+
 
 $(function() {
   $(window).load(function() {
-    App.init();
+	App.init();
   });
-  var interval = setInterval(App.dropPokemon, 60000);
 });
